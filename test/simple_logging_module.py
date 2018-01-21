97383c46aeef3fb742191109a46dc7cf04429206
wshe1 <2583276375@qq.com>
2018-01-21 10:49:04 +0800

    logger's using methods update

--- test/simple_logging_module.py
+++ test/simple_logging_module.py
@@ -2,7 +2,9 @@ import logging
 
 #create logger
 logger=logging.getLogger('simple_example')
+#logger=logging.basicConfig(filename='example.log',filemode='w')
 logger.setLevel(logging.DEBUG)
+
 
 #create console handler and set level to debug
 
@@ -12,12 +14,14 @@ ch.setLevel(logging.DEBUG)
 #create formatter
 formatter=logging.Formatter('%(asctime)s-%(name)s-%(levelname)s-%(message)s')
 
+#create log file
+fh=logging.FileHandler('simple_logging_module.log','w')
 #add formatter to ch
 ch.setFormatter(formatter)
 
-#add ch to logger
+#add ch and fh to logger
 logger.addHandler(ch)
-
+logger.addHandler(fh)
 # application
 logger.debug('debug')
 logger.info('info')
